553bfe875f82c9cafe9d8a0660306dfccaca5cc9
JB <jb.fiche@gmail.com>
2021-09-20 16:21:08 +0200

    Add piezo repositionning routine between each stage displacement

--- logic/tasks/roi_multicolor_scan_task_RAMM.py
+++ logic/tasks/roi_multicolor_scan_task_RAMM.py
@@ -139,6 +139,13 @@ class Task(InterruptableTask):  # do not change the name of the class. it is alw
         self.log.info('Moved to {} xy position'.format(self.roi_names[self.roi_counter]))
         self.ref['roi'].stage_wait_for_idle()
 
+        # reset piezo position to 25 um if too close to the limit of travel range (< 10 or > 50)
+        self.ref['focus'].do_piezo_position_correction()
+        busy = True
+        while busy:
+            sleep(0.5)
+            busy = self.ref['focus'].piezo_correction_running
+
         # autofocus
         self.ref['focus'].start_search_focus()
         # need to ensure that focus is stable here and stage is back at the sample surface, not on the reference plane
