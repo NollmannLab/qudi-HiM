5c2c4815060d08a49163198f98405de46af42887
fbarho <franziska.barho@gmx.fr>
2025-01-10 17:52:24 +0100

    Change model of filterwheel

--- hardware/wheels/thorlabs_motorized_filter_wheel.py
+++ hardware/wheels/thorlabs_motorized_filter_wheel.py
@@ -24,7 +24,7 @@ Copyright (c) the Qudi Developers. See the COPYRIGHT.txt file at the
 top-level directory of this distribution and at <https://github.com/Ulm-IQO/qudi/>
 -----------------------------------------------------------------------------------
 """
-import visa
+import pyvisa as visa
 from core.module import Base
 from interface.filterwheel_interface import FilterwheelInterface
 from core.configoption import ConfigOption
