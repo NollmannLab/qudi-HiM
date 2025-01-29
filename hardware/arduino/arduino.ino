f164b286f087fb9a45440d870f757925cf50df2a
JB <jb.fiche@gmail.com>
2025-01-29 18:51:47 +0100

    Allow digital writing & anologic reading

--- hardware/arduino/arduino.ino
+++ hardware/arduino/arduino.ino
@@ -1,106 +1,82 @@
-"""Qudi-CBS
-
-This script contains the code that has to be on the arduino device to use the arduino hardware.
-
-@author: D. Guerin, JB. Fiche
-
-Created on Tue may 18 2024.
------------------------------------------------------------------------------------
-
-Qudi is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-Qudi is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Qudi. If not, see <http://www.gnu.org/licenses/>.
-
-Copyright (c) the Qudi Developers. See the COPYRIGHT.txt file at the
-top-level directory of this distribution and at <https://github.com/Ulm-IQO/qudi/>
------------------------------------------------------------------------------------
-"""
-const int pin2 = 2;
-const int pin3 = 3;
-const int pin4 = 4;
-const int pin5 = 5;
-const int pin6 = 6;
-const int pin7 = 7;
-const int pin8 = 8;
-const int pin9 = 9;
-const int pin10 = 10;
-const int pin11 = 11;
-const int pin12 = 12;
-const int pin13 = 13;
+// const int pin2 = 2;
+// const int pin3 = 3;
+// const int pin4 = 4;
+// const int pin5 = 5;
+// const int pin6 = 6;
+// const int pin7 = 7;
+// const int pin8 = 8;
+// const int pin9 = 9;
+// const int pin10 = 10;
+// const int pin11 = 11;
+// const int pin12 = 12;
+// const int pin13 = 13;
 
 void setup() {
+  // Define the baud rate for the serial communication
   Serial.begin(9600);
-  pinMode(pin2, OUTPUT);
-  pinMode(pin3, OUTPUT);
-  pinMode(pin4, OUTPUT);
-  pinMode(pin5, OUTPUT);
-  pinMode(pin6, OUTPUT);
-  pinMode(pin7, OUTPUT);
-  pinMode(pin8, OUTPUT);
-  pinMode(pin9, OUTPUT);
-  pinMode(pin10, OUTPUT);
-  pinMode(pin11, OUTPUT);
-  pinMode(pin12, OUTPUT);
-  pinMode(pin13, OUTPUT);
 
-  digitalWrite(pin2, LOW);
-  digitalWrite(pin3, LOW);
-  digitalWrite(pin4, LOW);
-  digitalWrite(pin5, LOW);
-  digitalWrite(pin6, LOW);
-  digitalWrite(pin7, LOW);
-  digitalWrite(pin8, LOW);
-  digitalWrite(pin9, LOW);
-  digitalWrite(pin10, LOW);
-  digitalWrite(pin11, LOW);
-  digitalWrite(pin12, LOW);
-  digitalWrite(pin13, LOW);
+  // Set pins 2 to 13 as output - meaning they will sent digital signal to another device (for example the opto-coupler)
+  // Initialize pins as OUTPUT
+    for (int i = 2; i <= 13; i++) {
+        pinMode(i, OUTPUT);
+        digitalWrite(i, LOW);  // Set all to LOW initially
+    }
 }
 
 void loop() {
+  // Check if a command was sent from the computer
   if (Serial.available() > 0) {
     String command = Serial.readStringUntil('\n');
-    Serial.println("Received command: " + command); // Debugging line
-    if (command.length() == 2 || command.length() == 3) {
+    char firstChar = command.charAt(0);
+
+    // If command starts with a digit, the command will control a digital pin = change pin status
+    if (isDigit(firstChar)) {
       String pinStr = command.substring(0, command.length() - 1);
       char state = command.charAt(command.length() - 1);
+      int pin = pinStr.toInt();
 
-      int pinNumber = pinStr.toInt();
-
-      int arduinoPin;
-      switch(pinNumber) {
-        case 2: arduinoPin = pin2; break;
-        case 3: arduinoPin = pin3; break;
-        case 4: arduinoPin = pin4; break;
-        case 5: arduinoPin = pin5; break;
-        case 6: arduinoPin = pin6; break;
-        case 7: arduinoPin = pin7; break;
-        case 8: arduinoPin = pin8; break;
-        case 9: arduinoPin = pin9; break;
-        case 10: arduinoPin = pin10; break;
-        case 11: arduinoPin = pin11; break;
-        case 12: arduinoPin = pin12; break;
-        case 13: arduinoPin = pin13; break;
-        default: return;
+      // If input pin is valid, switch its state according to the indicated value from input command
+      if (pin >= 2 && pin <= 13) {
+          if (state == '1') {
+            digitalWrite(pin, HIGH);
+            Serial.println("pin" + String(pin) + " ON");
+          } else if (state == '0') {
+            digitalWrite(pin, LOW);
+            Serial.println("pin" + String(pin) + " OFF");
+          }
       }
-
-      if (state == '1') {
-        digitalWrite(arduinoPin, HIGH);
-        Serial.println("pin" + String(pinNumber) + " ON");
-      } else if (state == '0') {
-        digitalWrite(arduinoPin, LOW);
-        Serial.println("pin" + String(pinNumber) + " OFF");
-      }
+    }
+      // If command starts with a 'A', the command will control an analog input pin = read pin status
+    else if (firstChar == 'A') {
+        int analogPin = command.substring(1).toInt();
+        if (analogPin >= 0 && analogPin <= 5) {
+            int analogValue = analogRead(analogPin);
+            Serial.println(String(analogValue));
+        }
+        else {
+            Serial.println("Invalid analog pin");
+        }
+    }
+    else {
+        Serial.println("Unknown command format");
     }
   }
 }
 
+//       // Match arduinoPin to the pinNumber indicated in the input command. If the pin does not exist, return nothing
+//       int arduinoPin;
+//       switch(pinNumber) {
+//         case 2: arduinoPin = pin2; break;
+//         case 3: arduinoPin = pin3; break;
+//         case 4: arduinoPin = pin4; break;
+//         case 5: arduinoPin = pin5; break;
+//         case 6: arduinoPin = pin6; break;
+//         case 7: arduinoPin = pin7; break;
+//         case 8: arduinoPin = pin8; break;
+//         case 9: arduinoPin = pin9; break;
+//         case 10: arduinoPin = pin10; break;
+//         case 11: arduinoPin = pin11; break;
+//         case 12: arduinoPin = pin12; break;
+//         case 13: arduinoPin = pin13; break;
+//         default: return;
+//       }
